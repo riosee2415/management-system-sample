23ac548e6edd8d7996c7f2e3d9748c676f1c12c1
4leaf.ysh <4leaf.ysh@gmail.com>
2020-03-25 13:53:38 +0900

    used map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from "react";
-import logo from "./logo.svg";
+import Customer from "./components/Customer";
 import "./App.css";
+
+const customer = [
+  {
+    id: 1,
+    image:
+      "https://avatars2.githubusercontent.com/u/44108636?s=460&u=42afa365599d837455c04171238374a6e093cf08&v=4",
+    name: "윤상호",
+    birthday: "900224",
+    gender: "남자",
+    job: "개발자"
+  },
+  {
+    id: 2,
+    image:
+      "https://avatars2.githubusercontent.com/u/44108636?s=460&u=42afa365599d837455c04171238374a6e093cf08&v=4",
+    name: "김한주",
+    birthday: "971015",
+    gender: "여자",
+    job: "개발자"
+  }
+];
 
 class App extends React.Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Let's develop management system</h1>
+      <div>
+        {customer.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
       </div>
     );
   }
