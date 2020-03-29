920b86443b6bfca4095000a0dc86266d4214eabc
4leaf.ysh <4leaf.ysh@gmail.com>
2020-03-29 17:37:15 +0900

    Add coustomers API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
@@ -17,15 +18,21 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 });
 
 class App extends React.Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   };
 
   componentDidMount = () => {
+    this.timer = setInterval(this._progress, 20);
+
     this._callApi()
       .then(res =>
         this.setState({
@@ -40,6 +47,14 @@ class App extends React.Component {
     const body = await response.json();
 
     return body;
+  };
+
+  _progress = () => {
+    const { completed } = this.state;
+
+    this.setState({
+      completed: completed >= 100 ? 0 : completed + 1
+    });
   };
 
   render() {
@@ -60,21 +75,31 @@ class App extends React.Component {
           </TableHead>
 
           <TableBody>
-            {this.state.customers
-              ? this.state.customers.map(c => {
-                  return (
-                    <Customer
-                      key={c.id}
-                      id={c.id}
-                      image={c.image}
-                      name={c.name}
-                      birthday={c.birthday}
-                      gender={c.gender}
-                      job={c.job}
-                    />
-                  );
-                })
-              : ""}
+            {this.state.customers ? (
+              this.state.customers.map(c => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress
+                    className={classes.progress}
+                    variant="determinate"
+                    value={this.state.completed}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </Paper>
